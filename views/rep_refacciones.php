62b3ea4a553a8bcc20bd2c1d04dfae0949924ed3
fredy1617 <alfredomartinez6510@gmail.com>
2019-06-18 10:37:03 -0500

    refacciones
    Vista del reporte de refacciones

--- views/rep_refacciones.php
+++ views/rep_refacciones.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>SIC | Reporte de Refacciones</title>
+<?php
+include ('fredyNav.php');
+?>
 </head>
 <body>
 	<div class="container">
@@ -10,16 +13,31 @@
 			<h5 class="hide-on-large-only">Reporte de Refacciones:</h5>	
 		</div>
 		<div class="row">
-			<table>
+			<div class="col s12 l5 m5">
+                <label for="fecha_de">De:</label>
+                <input id="fecha_de" type="date" >    
+            </div>
+            <div class="col s12 l5 m5">
+                <label for="fecha_a">A:</label>
+                <input id="fecha_a" type="date" >
+            </div><br>
+            <div>
+                <button class="btn waves-light waves-effect right pink" onclick="buscar_refacciones();"><i class="material-icons prefix">send</i></button>
+            </div>
+		</div>
+		<div class="row">
+			<table lass="bordered highlight responsive-table" width="100%">
 				<thead>
 					<tr>
-						<th></th>
-						<th></th>
-						<th></th>
-						<th></th>
+						<th>ID</th>
+						<th>Descripcion</th>
+						<th>Cantidad</th>
+						<th>Cliente</th>
+						<th>Telefono</th>
+						<th>Dispositivo</th>
 					</tr>		
 				</thead>
-				<tbody>
+				<tbody id="datos">
 					
 				</tbody>
 			</table>			
